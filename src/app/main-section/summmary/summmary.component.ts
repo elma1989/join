1f2a049a9d6af6dbc38209454b5580dc51c54aaf
Prodigy2103 <marcusghne@gmx.de>
2025-09-26 14:09:01 +0200

    added all components

---
+++ src/app/main-section/summmary/summmary.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-summmary',
+  imports: [],
+  templateUrl: './summmary.component.html',
+  styleUrl: './summmary.component.scss'
+})
+export class SummmaryComponent {
+
+}
