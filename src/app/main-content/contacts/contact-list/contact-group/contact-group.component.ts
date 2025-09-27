cc715ff9ece4f4e6828bea012f6b43cadcdd8da2
Marco Elste <marco.elste@web.de>
2025-09-27 19:54:11 +0200

    chore: Added component ContactList, ContactGroup

--- src/app/main-content/contacts/contact-list/contact-group/contact-group.component.ts
+++ src/app/main-content/contacts/contact-list/contact-group/contact-group.component.ts
@@ -1,24 +1,11 @@
 import { Component, input, InputFunction } from '@angular/core';
-import { Contact } from '../../../../shared/classes/contact';
-import { DummyContactService } from '../dummy-contact.service';
-import { CommonModule } from '@angular/common';
-import { SingleContactComponent } from './single-contact/single-contact.component';
 
 @Component({
   selector: 'app-contact-group',
-  imports: [
-    CommonModule,
-    SingleContactComponent
-  ],
+  imports: [],
   templateUrl: './contact-group.component.html',
   styleUrl: './contact-group.component.scss'
 })
 export class ContactGroupComponent {
   public letter = input.required<string>();
-
-  constructor(private dcs:DummyContactService) {}
-
-  getContacts():Contact[] {
-    return this.dcs.getContactsByGroup(this.letter());
-  }
 }
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
+import { ContactsComponent } from './contacts/contacts.component';
 import { AsideComponent } from "./../../app/main-content/aside/aside.component";
 
 @Component({
   selector: 'app-main-content',
   imports: [
     HeaderComponent,
+    ContactsComponent,
     AsideComponent
 ],
   templateUrl: './main-content.component.html',
