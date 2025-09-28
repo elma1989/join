6a0365cdfb22d426db53b84f232da74974f776ba
Prodigy2103 <marcusghne@gmx.de>
2025-09-28 16:29:22 +0200

    changes in styles.scss, added nav-item.component

---
+++ src/app/shared/interfaces/navitemdata.ts
@@ -0,0 +1,5 @@
+export interface NavItemData {
+  sectionId: string; // Eindeutige ID zur Identifizierung der Sektion (z.B. 'Summary')
+  title: string;      // Die angezeigte Bezeichnung (z.B. 'Summary')
+  imagePath: string;  // Der Pfad zum Icon
+}
--- src/app/shared/nav-item/nav-item.component.ts
+++ src/app/shared/nav-item/nav-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { NavItemData } from './../interfaces/navitemdata';
 
 @Component({
   selector: 'app-nav-item',
@@ -8,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class NavItemComponent {
 
+  item = input.required<NavItemData>()
 }
