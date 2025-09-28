ff4fb66035bdd1f353f58252e7adce836c927cf4
Marco Elste <marco.elste@web.de>
2025-09-28 12:18:18 +0200

    chore: Updated component Header: Added comments to include Angular-Component as a vanilla-HTML-tag

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  /* Wenn der Selector so in eckige Klammern gesceiben wird,
+   * bedeutet das, die Komponente in ein normals Header-Tag eingebunden wird
+   * Der Name der Komponente app-header muss sich dabei vom Tag <header> unterscheien. */
   selector: 'header[app-header]',
+  // Wird so eingebunden: <header app-header></header>
   imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
