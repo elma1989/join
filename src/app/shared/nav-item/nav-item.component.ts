50a3efccbfdcafb56e5b26c68d70d12072db87e3
Prodigy2103 <marcusghne@gmx.de>
2025-09-28 13:43:03 +0200

    Changes style added nav-item components

---
+++ src/app/shared/nav-item/nav-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-item',
+  imports: [],
+  templateUrl: './nav-item.component.html',
+  styleUrl: './nav-item.component.scss'
+})
+export class NavItemComponent {
+
+}
