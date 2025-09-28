3fb57442a337bcbe84740d60f79dd082f1763a9a
Marco Elste <marco.elste@web.de>
2025-09-28 16:40:01 +0200

    chore: Updated component ContactList, ContactGroup: Added DummContact service

--- src/app/main-content/contacts/contact-list/contact-list.component.ts
+++ src/app/main-content/contacts/contact-list/contact-list.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ContactGroupComponent } from './contact-group/contact-group.component';
+import { DummyContactService } from './dummy-contact.service';
+import { Contact } from '../../../shared/classes/contact';
 
 @Component({
   selector: 'app-contact-list',
@@ -13,4 +15,10 @@ import { ContactGroupComponent } from './contact-group/contact-group.component';
 })
 export class ContactListComponent {
   protected groups:string[] = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  private contacts:Contact[];
+
+  constructor(private dcs:DummyContactService) {
+    this.contacts = dcs.contacts;
+  }
+
 }
