27cf447fd3a80033f6ea79440d439e4e9a71d33a
Rebell180 <buchmann_marcel@web.de>
2025-09-28 20:19:33 +0200

    implement fireContactService. icludes getAll, getOne, update, add and delete

---
+++ src/app/environment/environment.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA5PlY4eY4arHKrgMyXkQmdQqtbNwOgyl8", 
+    authDomain: "join-b08ef.firebaseapp.com", 
+    projectId: "join-b08ef", 
+    storageBucket: "join-b08ef.firebasestorage.app", 
+    messagingSenderId: "458487561187",
+    appId: "1:458487561187:web:a4195f2678309410b6fed6" 
+}
