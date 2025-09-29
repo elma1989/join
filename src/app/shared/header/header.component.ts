bde3241d35aa7afdd8ea41fe997abf08a17b8439
Prodigy2103 <marcusghne@gmx.de>
2025-09-29 12:25:36 +0200

    grid changes added aside component

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,38 +1,15 @@
 import { Component } from '@angular/core';
-import { NavItemData } from './../interfaces/navitemdata';
-import { NavItemComponent } from "../nav-item/nav-item.component";
+import { AsideComponent } from "../../main-content/aside/aside.component";
 
 @Component({
   /* Wenn der Selector so in eckige Klammern gesceiben wird,
    * bedeutet das, die Komponente in ein normales Header-Tag eingebunden wird
    * Der Name der Komponente app-header muss sich dabei vom Tag <header> unterscheien. */
   selector: 'header[app-header]',
-  imports: [NavItemComponent], 
+  imports: [AsideComponent], 
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   
-    item: Array<NavItemData> = [ 
-      {
-      sectionId: 'Summary',
-      title: 'Summary',
-      imagePath: 'assets/Icons/Summary.png'
-      },
-      {
-      sectionId: 'addTask',
-      title: 'Add task',
-      imagePath: 'assets/Icons/addTask.png'
-      },
-      {
-      sectionId: 'Boards', 
-      title: 'Boards',
-      imagePath: 'assets/Icons/Board.png'
-      },
-      {
-      sectionId: 'Contacts', 
-      title: 'Contacts',
-      imagePath: 'assets/Icons/Contacts.png'
-      }
-    ]
 }
