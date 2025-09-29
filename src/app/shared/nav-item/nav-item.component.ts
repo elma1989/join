bb7cd28b07a730dfb57435e9da3e0d4475bd9599
Prodigy2103 <marcusghne@gmx.de>
2025-09-29 13:29:45 +0200

    changes style

--- src/app/shared/nav-item/nav-item.component.ts
+++ src/app/shared/nav-item/nav-item.component.ts
@@ -2,7 +2,7 @@ import { Component, input } from '@angular/core';
 import { NavItemData } from './../interfaces/navitemdata';
 
 @Component({
-  selector: 'li[nav-item]',
+  selector: '[nav-item]',
   imports: [],
   templateUrl: './nav-item.component.html',
   styleUrl: './nav-item.component.scss'
