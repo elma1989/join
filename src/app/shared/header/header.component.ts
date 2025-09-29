e192d9afe5cefaeb30a2ec7157d7ad289a1c8834
Prodigy2103 <marcusghne@gmx.de>
2025-09-29 16:51:47 +0200

    all changes done

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { AsideComponent } from "../../main-content/aside/aside.component";
 
 @Component({
   /* Wenn der Selector so in eckige Klammern gesceiben wird,
    * bedeutet das, die Komponente in ein normales Header-Tag eingebunden wird
    * Der Name der Komponente app-header muss sich dabei vom Tag <header> unterscheien. */
   selector: 'header[app-header]',
-  imports: [AsideComponent], 
+  imports: [], 
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
