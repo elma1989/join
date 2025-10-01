54580022a6e27e23503028276523b9e0a732825e
Prodigy2103 <marcusghne@gmx.de>
2025-10-01 10:57:34 +0200

    app-contact-detail in contacts.component added and import in .ts

---
+++ src/app/main-content/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-detail',
+  imports: [],
+  templateUrl: './contact-detail.component.html',
+  styleUrl: './contact-detail.component.scss'
+})
+export class ContactDetailComponent {
+
+}
--- src/app/main-content/contacts/contacts.component.ts
+++ src/app/main-content/contacts/contacts.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactDetailComponent } from "./contact-detail/contact-detail.component";
 
 @Component({
   selector: 'section[contacts]',
   imports: [
-    ContactListComponent
-  ],
+    ContactListComponent,
+    ContactDetailComponent
+],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
