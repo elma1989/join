7d240394f802968f1936ed79acd5af584c69885a
Marco Elste <marco.elste@web.de>
2025-10-09 10:57:25 +0200

    chore: Updated display-size.sevice.ts - Created Enums for sizes

--- src/app/shared/services/display-size.service.ts
+++ src/app/shared/services/display-size.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
 
+export enum DisplayType {
+  SMART,
+  MOBILE,
+  TABLET,
+  NOTEBOOK,
+  DESKTOP,
+  BIGSCREEN
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class DisplaySizeService {
-
+  // #region Attributes
+  private sizes: {
+    smart: number,
+    mobile: number,
+    tablet: number,
+    notebook: number,
+    desktop: number,
+    bigscreen: number,
+  } =  {
+    smart: 450,
+    mobile: 640,
+    tablet: 768,
+    notebook: 1024,
+    desktop: 1025,
+    bigscreen: 1920,
+  }
+  // #endregion
   constructor() { }
 }
