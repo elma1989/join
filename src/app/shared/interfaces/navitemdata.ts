eee8bd5dbabe09047b267f471baa103de9028fd3
Marco Elste <marco.elste@web.de>
2025-10-12 08:38:21 +0200

    chore: Upadated navitemdata.ts - Insert JSDoc-Comments

--- src/app/shared/interfaces/navitemdata.ts
+++ src/app/shared/interfaces/navitemdata.ts
@@ -1,3 +1,4 @@
+/** Collects data for Nav-Item. */
 export interface NavItemData {
   sectionId: string; // Eindeutige ID zur Identifizierung der Sektion (z.B. 'Summary')
   title: string;      // Die angezeigte Bezeichnung (z.B. 'Summary')
