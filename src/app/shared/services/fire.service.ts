1316079247ecf8e9c4a5ca63564c941c2ca5ce65
Marco Elste <marco.elste@web.de>
2025-10-13 08:07:21 +0200

    feat: Updated fire.service.ts - FireService.getCollection

--- src/app/shared/services/fire.service.ts
+++ src/app/shared/services/fire.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Contact } from '../classes/contact';
+import { ContactGroup } from '../classes/contactGroup';
+import { collection, CollectionReference, Firestore } from '@angular/fire/firestore';
+
+type AllowedTypes = Contact | ContactGroup;
 
 @Injectable({
   providedIn: 'root'
 })
-export class FireService {
+export class FireService<T extends AllowedTypes> {
+
+  private fs:Firestore = inject(Firestore);
 
   constructor() { }
+
+  // #region methods
+  // #region references
+  /**
+   * Gets a collection from a path.
+   * @param path - Name of collection in database.
+   * @returns - Collection of path.
+   */
+  private getCollectionRef(path:string): CollectionReference {
+    return collection(this.fs, path);
+  }
+  // #endregion
 }
