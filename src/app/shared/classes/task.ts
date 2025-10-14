bbc1cdf7739c8eb7087c29fc801a01316040e07b
Prodigy2103 <marcusghne@gmx.de>
2025-10-14 14:56:52 +0200

    add task.ts and subTask.ts

---
+++ src/app/shared/classes/task.ts
@@ -0,0 +1,58 @@
+export class Task {
+
+	// #region Attributes
+	id: string;
+	title: string;
+	description: string;
+	dueDate: number;
+	priority: string;
+	category: string;
+	assignedTo: string;
+	subtasks: boolean = true;
+	// #endregion
+
+	constructor({id = '', title, description, dueDate, priority, category, assignedTo, subtasks}:{
+		id: string,
+		title: string,
+		description: string,
+		dueDate: number,
+		priority: string,
+		category: string,
+		assignedTo: string,
+		subtasks: boolean,
+	}) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.dueDate = dueDate;
+		this.priority = priority;
+		this.category = category;
+		this.assignedTo = assignedTo;
+		this.subtasks = subtasks;
+	}
+
+	/**
+ * Returns a plain JSON-compatible object representing the current Task instance.
+ *
+ * @returns {Object} An object containing all Task properties.
+ *
+ * @description
+ * - convert the Task into a JSON string.
+ * - || operator provides default fallback values if a property is `undefined` or empty.
+ * - ?? mullish operator ensures that `false` for `subtasks`
+ *   is preserved and not overwritten by the default value.
+ */
+
+	toJsonTask() {
+		return {
+			id: this.id || "",
+			title: this.title || "",
+			description: this.description || "",
+			dueDate: this.dueDate || 0,
+			priority: this.priority || "",
+			category: this.category || "",
+			assignedTo: this.assignedTo || "",
+			subtasks: this.subtasks ?? true
+		}
+	}
+}
