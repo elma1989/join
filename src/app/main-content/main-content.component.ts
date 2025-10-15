91635439c3bd4c6490502c5da59216819831ff06
Marco Elste <marco.elste@web.de>
2025-10-15 19:21:09 +0200

    style: Updated board.component.scss - Styling Desktop

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,13 +3,15 @@ import { HeaderComponent } from '../shared/header/header.component';
 import { AsideComponent } from "./../../app/main-content/aside/aside.component";
 import { ContactsComponent } from './contacts/contacts.component';
 import { SectionType } from '../shared/enums/section-type';
+import { BoardComponent } from './board/board.component';
 
 @Component({
   selector: 'app-main-content',
   imports: [
     HeaderComponent,
     AsideComponent,
-    ContactsComponent
+    ContactsComponent,
+    BoardComponent
 ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
