b485c434805993df8907e83b7f7c4811f62aaeae
Marco Elste <marco.elste@web.de>
2025-10-15 14:53:36 +0200

    style: Updated nav-item.component.scss - Styled clicked NavItem

--- src/app/main-content/aside/aside.component.ts
+++ src/app/main-content/aside/aside.component.ts
@@ -1,50 +1,61 @@
 import { Component, output, OutputEmitterRef } from '@angular/core';
-import { NavItemComponent } from '../../shared/nav-item/nav-item.component';
-import { NavItemData } from '../../shared/interfaces/navitemdata';
 import { SectionType } from '../../shared/enums/section-type';
-import { throwIfEmpty } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { NavItemComponent } from './nav-item/nav-item.component';
+
+
+export interface NavItemData{
+    sectionId: string,
+    title: string,
+    imagePath: string,
+    section: SectionType,
+    active: boolean
+}
 
 @Component({
   selector: 'aside[app-aside]',
-  imports: [NavItemComponent],
+  imports: [CommonModule, NavItemComponent],
   templateUrl: './aside.component.html',
   styleUrl: './aside.component.scss'
 })
 export class AsideComponent {
-  protected items: {
-    sectionId: string,
-    title: string,
-    imagePath: string,
-    section: SectionType
-  }[] = [ 
+  protected items: NavItemData[] = [ 
       {
       sectionId: 'Summary',
       title: 'Summary',
       imagePath: 'assets/Icons/Summary.png',
-      section: SectionType.SUMMARY
+      section: SectionType.SUMMARY,
+      active: false
       },
       {
       sectionId: 'addTask',
       title: 'Add task',
       imagePath: 'assets/Icons/addTask.png',
-      section: SectionType.TASK
+      section: SectionType.TASK,
+      active: false,
       },
       {
       sectionId: 'Boards', 
       title: 'Boards',
       imagePath: 'assets/Icons/Board.png',
-      section: SectionType.BOARD
+      section: SectionType.BOARD,
+      active: true,
       },
       {
       sectionId: 'Contacts', 
       title: 'Contacts',
       imagePath: 'assets/Icons/Contacts.png',
-      section: SectionType.CONTACT
+      section: SectionType.CONTACT,
+      active: false
       }
     ];
     selectedSection: OutputEmitterRef<SectionType> = output<SectionType>();
 
     selectSection(index:number) {
+      this.items.forEach((item, i) => {
+        item.active = false;
+        if (i == index) item.active = true;
+      });
       this.selectedSection.emit(this.items[index].section)
     }
 }
