e3b5d4b8e58837082e3137175966fe38e00fcad4
Marco Elste <marco.elste@web.de>
2025-10-15 14:11:29 +0200

    feat: Upadated component Aside - Aside.selectSection()

--- src/app/main-content/aside/aside.component.ts
+++ src/app/main-content/aside/aside.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, output, OutputEmitterRef } from '@angular/core';
 import { NavItemComponent } from '../../shared/nav-item/nav-item.component';
 import { NavItemData } from '../../shared/interfaces/navitemdata';
+import { SectionType } from '../../shared/enums/section-type';
+import { throwIfEmpty } from 'rxjs';
 
 @Component({
   selector: 'aside[app-aside]',
@@ -9,26 +11,40 @@ import { NavItemData } from '../../shared/interfaces/navitemdata';
   styleUrl: './aside.component.scss'
 })
 export class AsideComponent {
-  item: Array<NavItemData> = [ 
+  protected items: {
+    sectionId: string,
+    title: string,
+    imagePath: string,
+    section: SectionType
+  }[] = [ 
       {
       sectionId: 'Summary',
       title: 'Summary',
-      imagePath: 'assets/Icons/Summary.png'
+      imagePath: 'assets/Icons/Summary.png',
+      section: SectionType.SUMMARY
       },
       {
       sectionId: 'addTask',
       title: 'Add task',
-      imagePath: 'assets/Icons/addTask.png'
+      imagePath: 'assets/Icons/addTask.png',
+      section: SectionType.TASK
       },
       {
       sectionId: 'Boards', 
       title: 'Boards',
-      imagePath: 'assets/Icons/Board.png'
+      imagePath: 'assets/Icons/Board.png',
+      section: SectionType.BOARD
       },
       {
       sectionId: 'Contacts', 
       title: 'Contacts',
-      imagePath: 'assets/Icons/Contacts.png'
+      imagePath: 'assets/Icons/Contacts.png',
+      section: SectionType.CONTACT
       }
-    ]
+    ];
+    selectedSection: OutputEmitterRef<SectionType> = output<SectionType>();
+
+    selectSection(index:number) {
+      this.selectedSection.emit(this.items[index].section)
+    }
 }
