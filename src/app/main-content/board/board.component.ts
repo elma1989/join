48a6a13cf3a2d45703be82635d2f48e1e9071399
Marco Elste <marco.elste@web.de>
2025-10-16 18:43:32 +0200

    feat: Updated Component Board - BoardComponent.filterTasks(), BoardComponent.getTasks()

--- src/app/main-content/board/board.component.ts
+++ src/app/main-content/board/board.component.ts
@@ -1,21 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SearchTaskComponent } from './search-task/search-task.component';
+import { Task } from '../../shared/classes/task';
+import { FirebaseDBService } from '../../shared/services/firebase-db.service';
+import { Unsubscribe } from '@angular/fire/firestore';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'section[board]',
   imports: [
-    SearchTaskComponent
+    SearchTaskComponent,
+    CommonModule
   ],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
-export class BoardComponent {
-  
-    /**
-     * Filters all Tasks by user input.
-     * @param userSearch - Input from User-Searchbar.
-     */
-    filterTasks(userSearch:string) {
-      console.log(userSearch);
-    }
+export class BoardComponent implements OnInit{
+
+  shownTasks!: Task[];
+  tasksSnapshot!: Unsubscribe
+
+  constructor(private fdb:FirebaseDBService) {}
+
+  ngOnInit(): void {
+    this.shownTasks = this.fdb.tasks;
+  }
+
+  /**
+   * Filters all Tasks by user input.
+   * @param userSearch - Input from User-Searchbar.
+   */
+  filterTasks(userSearch: string) {
+    this.shownTasks = userSearch.length == 0 ? this.fdb.tasks : this.fdb.tasks.filter(task => task.title.includes(userSearch));
+  }
+
+  /** Gets all Tasks, which has been searched.
+   * If user is not looking for tasks, all tasks are schown.
+   */
+  getTasks() {
+    return this.shownTasks;
+  }
 }
