2cfcd200965ff1f09eaeaf3e3ee94e0bbf8d2d12
Marco Elste <marco.elste@web.de>
2025-10-17 13:32:40 +0200

    chore: Updated task-list-column.component.html - structure of list

--- src/app/main-content/board/task-list-column/task-list-column.component.ts
+++ src/app/main-content/board/task-list-column/task-list-column.component.ts
@@ -1,9 +1,12 @@
 import { Component, input, InputSignal } from '@angular/core';
 import { Task } from '../../../shared/classes/task';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-list-column',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './task-list-column.component.html',
   styleUrl: './task-list-column.component.scss'
 })
