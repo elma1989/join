23a9635cf0e91d6825d4ece45a5ad01e3a88b6c3
Rebell180 <buchmann_marcel@web.de>
2025-10-17 07:38:18 +0200

    add all elements to add-task and pre-define contacts to assignedTo field.

--- src/app/shared/components/add-task/add-task.component.ts
+++ src/app/shared/components/add-task/add-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy,  } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FirebaseDBService } from '../../services/firebase-db.service';
 import { Priority } from '../../enums/priority.enum';
@@ -6,6 +6,8 @@ import { Task } from '../../classes/task';
 import { CommonModule } from '@angular/common';
 import { PriorityButtonsComponent } from "../priority-buttons/priority-buttons.component";
 import { Category } from '../../enums/category.enum';
+import { query, Unsubscribe, where, Query, onSnapshot } from '@angular/fire/firestore';
+import { Contact } from '../../classes/contact';
 
 
 @Component({
@@ -15,18 +17,39 @@ import { Category } from '../../enums/category.enum';
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
-export class AddtaskComponent implements OnInit {
+export class AddtaskComponent implements OnDestroy {
 
   fireDB: FirebaseDBService = inject(FirebaseDBService);
   Priority = Priority;
   Category = Category;
 
   currentTask: Task = new Task();
+  contacts: Array<Contact> = []
 
+  unsubContacts: Unsubscribe;
+
+  constructor() {
+    this.unsubContacts = this.getContactsSnapshot();
+  }
+
+  ngOnDestroy() {
+    this.unsubContacts();
+  }
+
+  /**
+   * Opens a two way data stream between code and firebase collection 'contacts'.
+   * 
+   * @returns an @type Unsubscribe.
+   */
+  private getContactsSnapshot(): Unsubscribe {
+    const q: Query = query(this.fireDB.getCollectionRef('contacts'), where('id', '!=', ''));
   
-
-  ngOnInit() {
-
+    return onSnapshot(q, (list) => {
+      this.contacts = [];
+      list.forEach((docRef) => {
+        this.contacts.push(this.fireDB.mapResponseToContact({ ...docRef.data(), id: docRef.id}));
+      });
+    });
   }
 
   /**
@@ -45,6 +68,12 @@ export class AddtaskComponent implements OnInit {
     console.log(this.currentTask.priority);
   }
 
-  
+  toggleAddContactToAssignTo(id: string) {
+    if(this.currentTask.assignedTo.includes(id)){
+      // remove
+    } else {
+      this.currentTask.assignedTo.push(id);
+    }
+  }
 
 }
