bc44545acdbc177e040446adb1067af3301f8e59
Marco Elste <marco.elste@web.de>
2025-10-22 13:01:21 +0200

    feat: Updated SubtaskComponent - SubtaskComponent.selectEditImput()

--- src/app/main-content/board/subtask/subtask.component.ts
+++ src/app/main-content/board/subtask/subtask.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, input, InputSignal, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, inject, input, InputSignal, ViewChild } from '@angular/core';
 import { Task } from '../../../shared/classes/task';
 import { SubTask } from '../../../shared/classes/subTask';
 import { FormsModule } from '@angular/forms';
@@ -15,16 +15,38 @@ import { FormsModule } from '@angular/forms';
 })
 export class SubtaskComponent {
   task: InputSignal<Task> = input.required<Task>();
+  cdr: ChangeDetectorRef = inject(ChangeDetectorRef);
   @ViewChild('editsub') editsub!: ElementRef<HTMLInputElement>;
 
   /**
    * Enables the Edit-Mode of Subtask.
    * @param psubtask - Instance of SubTask.
    */
-  endbleEdit(psubtask: SubTask) {
-    this.task().subtasks.forEach(subtask => {
-      subtask.editMode = false;
-      if (subtask.id == psubtask.id) subtask.editMode = true;
-    });
+  private endbleEdit(psubtask: SubTask): void {
+    let editEnabled: boolean = this.task().subtasks.every(subtask => !subtask.editMode)
+    if (editEnabled) {
+      this.task().subtasks.forEach(subtask => {
+        if (subtask.id == psubtask.id) subtask.editMode = true;
+      });
+    }
+  }
+
+  /** Sets focus on edit subtask input */
+  private focusEdit(): void {
+    setTimeout(() => {
+      this.cdr.detectChanges();
+      const el = this.editsub.nativeElement;
+      el.focus();
+      el.select();
+    })
+  }
+
+  /**
+   * Enables SubtaskEdit-Mode and sets focus on input-field.
+   * @param subtask - Instance of SubTask.
+   */
+  protected selectEditInput(subtask: SubTask) {
+    this.endbleEdit(subtask);
+    this.focusEdit();
   }
 }
