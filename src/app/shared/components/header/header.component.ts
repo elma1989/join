51f0913748e99af076bd7b16f14ffba0204ffc3a
Prodigy2103 <marcusghne@gmx.de>
2025-10-24 16:36:44 +0200

    add dropdownmenu in smallheader

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject} from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { ModalService } from '../../services/modal.service';
 
 @Component({
@@ -8,10 +8,30 @@ import { ModalService } from '../../services/modal.service';
    * Der Name der Komponente app-header muss sich dabei vom Tag <header> unterscheien. */
   selector: 'header[app-header]',
   standalone: true,
-  imports: [CommonModule], 
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   protected modalService: ModalService = inject(ModalService);
+
+  isMenuVisible: boolean = false;
+
+  toggleMenu(): void {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
+
+  logout(): void {
+    // Hier deine Logout-Logik einbauen
+    console.log('User logged out');
+    this.isMenuVisible = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event): void {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.profile-container')) {
+      this.isMenuVisible = false;
+    }
+  }
 }
