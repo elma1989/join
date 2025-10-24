73fc69f86b55f9e10e9c307ec91d2819ffe09c5d
Prodigy2103 <marcusghne@gmx.de>
2025-10-24 09:56:07 +0200

    add new mixin text-style adjusted code rows in @include add @viewChild(), ngAfterViewInit and other

--- src/app/main-content/board/task-list-column/task-list-column.component.ts
+++ src/app/main-content/board/task-list-column/task-list-column.component.ts
@@ -1,19 +1,19 @@
-import { Component, inject, input, InputSignal } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, input, InputSignal, Renderer2, ViewChild } from '@angular/core';
 import { Task } from '../../../shared/classes/task';
 import { CommonModule } from '@angular/common';
 import { TaskColumnItemComponent } from '../../../shared/components/task-column-item/task-column-item.component';
-import { TaskModalComponent } from "../../../shared/components/modals/task-modal/task-modal.component";
 
 @Component({
-  selector: 'app-task-list-column',
-  imports: [
-    CommonModule,
-    TaskColumnItemComponent,
-],
-  templateUrl: './task-list-column.component.html',
-  styleUrl: './task-list-column.component.scss'
+    selector: 'app-task-list-column',
+    imports: [
+        CommonModule,
+        TaskColumnItemComponent,
+    ],
+    templateUrl: './task-list-column.component.html',
+    styleUrl: './task-list-column.component.scss'
 })
 export class TaskListColumnComponent {
-  listName: InputSignal<string> = input.required<string>();
-  tasks: InputSignal<Task[]> = input.required<Task[]>()
+
+    listName: InputSignal<string> = input.required<string>();
+    tasks: InputSignal<Task[]> = input.required<Task[]>();
 }
