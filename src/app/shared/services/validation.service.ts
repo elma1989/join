1a1acea1d3f0d071db8f0e35676d1e18ef96a8a3
Marco Elste <marco.elste@web.de>
2025-11-01 10:57:11 +0100

    feat: Updated ValidationService - ValidationService.registerForm()

--- src/app/shared/services/validation.service.ts
+++ src/app/shared/services/validation.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ValidationService {
 
-  constructor() { }
+  forms: Map<string, FormGroup> = new Map<string, FormGroup>();
+
+  // #region Methods
+  // #region Form-Mangement
+  /**
+   * Adds a new from for registry.
+   * @param key - Key of form.
+   * @param form - FromGroup, which is added.
+   */
+  registerForm(key: 'contact', form: FormGroup) {
+    this.forms.set(key, form);
+  }
+  // #endregion
+  // #endregion
 }
