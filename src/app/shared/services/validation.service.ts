0bf9878ce42caac33090e9e4b6db31db3db08604
Marco Elste <marco.elste@web.de>
2025-11-06 11:16:47 +0100

    feat: Upadated ValiationService - collectErrors(), pushErrorByControl() - recursive form control

--- src/app/shared/services/validation.service.ts
+++ src/app/shared/services/validation.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -26,7 +26,7 @@ export class ValidationService {
   removeForm(key: string): void {
     this.forms.delete(key);
   }
-  
+
   /**
    * Validates a form.
    * @param formType - Type of Form.
@@ -37,26 +37,53 @@ export class ValidationService {
     const form: FormGroup | undefined = this.forms.get(formType);
 
     if (form) {
-      Object.keys(form.controls).forEach(key => {
-        const control = form.get(key);
-        if (control && control.errors) {
-          errors[key] = [];
-          for (const errorKey in control.errors) {
-            errors[key].push(this.getErrorMessage(errorKey, control.errors[errorKey]))
-          }
-        }
-      });
+      this.collectErrors(form, errors);
     }
-
     return errors
   }
+
+  /**
+   * Collects errors in forms recursively.
+   * @param control - Control to check.
+   * @param errors - Record of errors.
+   * @param path - Path of pareent or child
+   */
+  private collectErrors(control: AbstractControl, errors: Record<string, string[]>, path: string = ''): void {
+    if (control instanceof FormGroup) {
+      Object.keys(control.controls).forEach(key => {
+        const child = control.get(key);
+        if (child) {
+          const childPath = path ? `${path}.${key}` : key;
+          this.collectErrors(child, errors, childPath);
+        }
+      });
+    } else if (control instanceof FormControl) {
+      this.pushErrorByControl(control, errors, path);
+    }
+  }
+
+  /**
+   * Pushs error from a control in the record of errors.
+   * @param control - Instance of FormContol.
+   * @param errors - Record of errors.
+   * @param path - Path to push.
+   */
+  private pushErrorByControl(control: FormControl, errors: Record<string, string[]>, path: string): void {
+    if (control.errors) {
+      errors[path] = [];
+      for (const errorKey in control.errors) {
+        errors[path].push(this.getErrorMessage(errorKey, control.errors[errorKey]))
+      }
+    }
+  }
+
   /**
    * Gets an error message for user.
    * @param errorKey - Key in Error-Record
    * @param errorValue - Value of Validation (example maxLength(value))
    * @returns - Errormessage for user.
    */
-  private getErrorMessage(errorKey: string, errorValue?:any): string {
+  private getErrorMessage(errorKey: string, errorValue?: any): string {
     switch (errorKey) {
       case 'strictRequired':
         return 'Field is required.';
