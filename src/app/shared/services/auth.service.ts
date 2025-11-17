ef3d05610909a5b3502904feb50009f741d15c1e
Marco Elste <marco.elste@web.de>
2025-11-17 16:34:28 +0100

    feat: Update AuthService - login()

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { inject, Injectable } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, onAuthStateChanged, UserCredential } from '@angular/fire/auth';
+import { Auth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, UserCredential } from '@angular/fire/auth';
 import { User } from '../classes/user';
+import { LoginData } from '../interfaces/login-data';
+import { Firestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +10,31 @@ import { User } from '../classes/user';
 export class AuthService {
 
   private auth: Auth = inject(Auth);
+  private fs: Firestore = inject(Firestore)
 
   /**
    * Registers a user.
    * @param email - E-Mail of user.
    * @param password Password of user.
-   * @returns User-Credential as Observable
+   * @returns User-Credentials.
    */
   async register(user: User): Promise<UserCredential> {
     const cred = await createUserWithEmailAndPassword(this.auth, user.email, user.password);
     user.id = cred.user.uid;
     return cred;
+  }
+
+  /**
+   * Signs a user in.
+   * @param data - Fromdata from login.
+   * @returns User-Credentials.
+   */
+  async login(data: LoginData): Promise<UserCredential> {
+    try {
+      const cred = await signInWithEmailAndPassword(this.auth, data.email, data.password);
+      return cred;
+    } catch (err) {
+      throw err;
+    }
   }
 }
