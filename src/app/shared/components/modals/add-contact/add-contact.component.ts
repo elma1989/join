53ca03a9bef1bf6d2c73803726befdde077a64ff
Prodigy2103 <marcusghne@gmx.de>
2025-11-21 16:24:35 +0100

    console.log to testing db traffic

--- src/app/shared/components/modals/add-contact/add-contact.component.ts
+++ src/app/shared/components/modals/add-contact/add-contact.component.ts
@@ -92,7 +92,7 @@ export class AddContactComponent implements OnInit, AfterViewInit, OnDestroy {
     this.contactForm.setValue(formdata);
     this.val.registerForm('contact', this.contactForm);
     this.subFormCahange = this.contactForm.valueChanges.subscribe(() => {this.validateForm();});
-    
+    console.log('Original Contact ID (ngOnInit):', this.contact().id);
   }
 
   ngAfterViewInit() {
@@ -148,12 +148,10 @@ export class AddContactComponent implements OnInit, AfterViewInit, OnDestroy {
       }
       this.closeModal();
     }
+    console.log('Original Contact ID (ngOnInit):', this.contact().id);
   }
   // #endregion
   
-  async updateContact() {
-    
-  }
   /**
    * Closes the modal
    * 
