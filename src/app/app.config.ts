0cce0aea879adf3a3b87f17d81077ec31b99279f
Marco Elste <marco.elste@web.de>
2026-02-09 09:53:20 +0100

    chore: Updated app.config.ts - Added new firestore database

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,5 +15,5 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideFirebaseApp(() => initializeApp( firebaseConfig )), 
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())]
+    provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({ projectId: "join-6c719", appId: "1:576560983875:web:f179ad1718b2e48709c9f6", storageBucket: "join-6c719.firebasestorage.app", apiKey: "AIzaSyDP5Odb2B6cUv6N0GzSyXV5lN6JKE9DHxg", authDomain: "join-6c719.firebaseapp.com", messagingSenderId: "576560983875", measurementId: "G-YRD88CLEWR", projectNumber: "576560983875", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
