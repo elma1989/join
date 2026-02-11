f2a21e967c18b88ee7d24f25a31437f2e86c4e1a
Marco Elste <marco.elste@web.de>
2026-02-11 09:49:13 +0100

    fix: Updated app.config.ts - App was not built after change database - fixed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { firebaseConfig } from './environment/environment';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
@@ -10,10 +9,17 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter( routes ), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
     provideAnimations(),
-    provideFirebaseApp(() => initializeApp( firebaseConfig )), 
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({ projectId: "join-6c719", appId: "1:576560983875:web:f179ad1718b2e48709c9f6", storageBucket: "join-6c719.firebasestorage.app", apiKey: "AIzaSyDP5Odb2B6cUv6N0GzSyXV5lN6JKE9DHxg", authDomain: "join-6c719.firebaseapp.com", messagingSenderId: "576560983875", measurementId: "G-YRD88CLEWR", projectNumber: "576560983875", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+
+    provideFirebaseApp(() => initializeApp({ projectId: "join-6c719", 
+      appId: "1:576560983875:web:f179ad1718b2e48709c9f6", 
+      storageBucket: "join-6c719.firebasestorage.app", 
+      apiKey: "AIzaSyDP5Odb2B6cUv6N0GzSyXV5lN6JKE9DHxg", 
+      authDomain: "join-6c719.firebaseapp.com", 
+      messagingSenderId: "576560983875", 
+      measurementId: "G-YRD88CLEWR" })), 
+    provideAuth(() => getAuth()), 
+    provideFirestore(() => getFirestore())]
 };
